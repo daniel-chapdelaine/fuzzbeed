30a6c5b827ba4ef10128418a43a3e4f4d84bf0cc
Daniel Chapdelaine <orvist@gmail.com>
2025-09-20 17:36:35 -0500

    fixing broken assets

--- src/Related.jsx
+++ src/Related.jsx
@@ -22,21 +22,21 @@ function Related() {
       <em>Related</em>
       <div className='articles-container'>
         <ArticleLink
-          imgSrc="./src/assets/gnome.jpg"
+          imgSrc={import.meta.env.VITE_ASSET_PATH + 'assets/gnome.jpg'}
           href="https://dungeonmister.com/guides/races-in-dungeons-dragons/deep-gnome-svirfneblin-race-in-dnd-5e/"
           title="Deep Gnome (Svirfneblin) Playable Race Guide for D&D 5e 2014"
           date="January, 11th 2023"
         />
   
         <ArticleLink
-          imgSrc="./src/assets/mining.jpg"
+          imgSrc={import.meta.env.VITE_ASSET_PATH + 'assets/mining.jpg'}
           href="https://www.thegamer.com/stardew-valley-fast-mining-level-up-tips/"
           title="How To Quickly Level Up Mining In Stardew Valley"
           date="March 4, 2024"
         />
 
         <ArticleLink
-          imgSrc="./src/assets/trickster.jpg"
+          imgSrc={import.meta.env.VITE_ASSET_PATH + 'assets/trickster.jpg'}
           href="https://dungeonmister.com/guides/classes-in-dungeons-dragons/arcane-trickster-spell-guide-dnd-2024/#:~:text=The%20arcane%20trickster%20is%20a,of%20a%201%2F3%20caster."
           title="The Best Arcane trickster Spells for D&D 2024 (5.5e)"
           date="January, 12th 2025"
